e5701c17b860bc1fe06923fee9b53ffab1fe44db
Prince Bhesaniya <126847549+PrinceBhesaniya28@users.noreply.github.com>
2025-02-04 17:12:16 +0530

    next things

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,36 @@
 import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+const config: Config = {
+    content: [
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        screens: {
+            sm: '375px',
+            md: '768px',
+            lg: '1200px',
+        }, 
+        extend: {
+            fontfamily: {
+               sans: 'var(--font-archivo/)' 
+            },
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: "1rem",
+                    md: "2rem",
+                    lg: "4rem",
+                },
+            },
+            colors: {
+                "red-orange": {
+                    500: 'color-mix(in srgb, theme("colors.red.500") 50%, theme("colors.orange.500") 50%)',
+                },
+            },
+        },
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: [],
+};
+export default config;
